ff819d8e9202a2c08ddfdf2cd3c28a99ca847361
Robbie Jaeger <jaeger.rob@gmail.com>
2017-05-29 19:56:22 -0600

    Fix indentation

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-
 $submitBtn = $('.new-item-form--submit');
-	$itemInput = $('.new-item-form--name');
-	$itemList = $('.item-list');
+$itemInput = $('.new-item-form--name');
+$itemList = $('.item-list');
 
 
-	$submitBtn.on('click', function(e){
-		e.preventDefault();
-		inputText = $itemInput.val();
-		$itemList.append(
-			`<article class="item-list-item">
-				<p>${inputText}</p>
-				<button class="delete-item">Delete</button>
-			</article>`
-			);
-		$itemInput.val('');
-		$submitBtn.prop('disabled', true);
-	});
+$submitBtn.on('click', function(e){
+  e.preventDefault();
+  inputText = $itemInput.val();
+  $itemList.append(
+    `<article class="item-list-item">
+      <p>${inputText}</p>
+      <button class="delete-item">Delete</button>
+    </article>`
+  );
+  $itemInput.val('');
+  $submitBtn.prop('disabled', true);
+});
 
 
-	$itemInput.on('input', function(){
-		if ($(this).val() === '') {
-			$submitBtn.prop('disabled', true);
-		} else {
-			$submitBtn.prop('disabled', false);
-		}
-	});
+$itemInput.on('input', function(){
+  if ($(this).val() === '') {
+    $submitBtn.prop('disabled', true);
+  } else {
+    $submitBtn.prop('disabled', false);
+  }
+});
 
 
-	$itemList.on('click', '.delete-item', function(){
-		$(this).parent().remove();
-	});
+$itemList.on('click', '.delete-item', function(){
+  $(this).parent().remove();
+});
