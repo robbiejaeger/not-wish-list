8f18bf1e7fc1be05ca4504aec2ce0ca5175029ac
Robbie Jaeger <jaeger.rob@gmail.com>
2017-05-29 19:54:45 -0600

    Add live code

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 
+$submitBtn = $('.new-item-form--submit');
+	$itemInput = $('.new-item-form--name');
+	$itemList = $('.item-list');
+
+
+	$submitBtn.on('click', function(e){
+		e.preventDefault();
+		inputText = $itemInput.val();
+		$itemList.append(
+			`<article class="item-list-item">
+				<p>${inputText}</p>
+				<button class="delete-item">Delete</button>
+			</article>`
+			);
+		$itemInput.val('');
+		$submitBtn.prop('disabled', true);
+	});
+
+
+	$itemInput.on('input', function(){
+		if ($(this).val() === '') {
+			$submitBtn.prop('disabled', true);
+		} else {
+			$submitBtn.prop('disabled', false);
+		}
+	});
+
+
+	$itemList.on('click', '.delete-item', function(){
+		$(this).parent().remove();
+	});
